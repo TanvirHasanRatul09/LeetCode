b72e9a21b65c22e36a9b7d5032317ceed42366c7
Tanvir Hasan Ratul <tanvirhasan58p@gmail.com>
2026-07-27 01:37:01 +0600

    Time: 0 ms (100%), Space: 12.1 MB (11.95%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>f;
-        for (int c:magazine){
-            f[c]++;
+
+        unordered_map<char, int> rFreq;
+        unordered_map<char, int> mFreq;
+
+        // Count ransomNote
+        for (char c : ransomNote) {
+            rFreq[c]++;
         }
-        for(int c:ransomNote){
-            if(f[c]==0){
+
+        // Count magazine
+        for (char c : magazine) {
+            mFreq[c]++;
+        }
+
+        // Compare frequencies
+        for (auto it : rFreq) {
+            if (mFreq[it.first] < it.second) {
                 return false;
             }
-            f[c]--;
         }
+
         return true;
     }
 };
