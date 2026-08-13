cf42e8608b21dfa72b69adcb1dc76eda2527e88b
Tanvir Hasan Ratul <tanvirhasan58p@gmail.com>
2026-08-13 22:33:55 +0600

    Time: 6 ms (24.48%), Space: 15.2 MB (18.83%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,104 +2,25 @@ class Solution {
 public:
     string simplifyPath(string path) {
 
-        stack<char> st;
-        int dotCount = 0;
-        bool hasOther = false;
-
-        for(char ch : path) {
-
-            // When a component ends
-            if(ch == '/') {
-
-                // "."
-                if(!hasOther && dotCount == 1) {
-
-                    // remove the '.'
-                    if(!st.empty())
-                        st.pop();
+        stack<string> st;
+        stringstream ss(path);
+        string dir;
+        while(getline(ss,dir,'/')){
+            if(dir.empty() || dir=="."){
+                continue;
+            }else if(dir==".."){
+                if(!st.empty()){
+                    st.pop();
                 }
-
-                // ".."
-                else if(!hasOther && dotCount == 2) {
-
-                    // remove the two dots
-                    if(!st.empty()) st.pop();
-                    if(!st.empty()) st.pop();
-
-                    // remove '/' before ..
-                    if(!st.empty() && st.top() == '/')
-                        st.pop();
-
-                    // remove previous directory
-                    while(!st.empty() && st.top() != '/') {
-                        st.pop();
-                    }
-                }
-
-                // reset current component
-                dotCount = 0;
-                hasOther = false;
-
-                // avoid multiple '/'
-                if(!st.empty() && st.top() == '/')
-                    continue;
-
-                st.push('/');
-            }
-
-            else {
-
-                st.push(ch);
-
-                if(ch == '.') {
-                    dotCount++;
-                }
-                else {
-                    hasOther = true;
-                }
+            }else{
+                st.push(dir);
             }
         }
-
-        // Handle the last component
-        if(!hasOther && dotCount == 1) {
-
-            if(!st.empty())
-                st.pop();
-        }
-
-        else if(!hasOther && dotCount == 2) {
-
-            // remove ".."
-            if(!st.empty()) st.pop();
-            if(!st.empty()) st.pop();
-
-            // remove '/'
-            if(!st.empty() && st.top() == '/')
-                st.pop();
-
-            // remove previous directory
-            while(!st.empty() && st.top() != '/') {
-                st.pop();
-            }
-        }
-
-        // Remove final '/'
-        if(!st.empty() && st.top() == '/') {
+        string res;
+        while(!st.empty()){
+            res="/"+st.top()+res;
             st.pop();
         }
-
-        string ans;
-
-        while(!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
-
-        if(ans.empty())
-            return "/";
-
-        return ans;
+        return res.empty()?"/":res;
     }
 };
