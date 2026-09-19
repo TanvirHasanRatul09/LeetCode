43600b1c14f955c638daed93f00d15c5ada7f286
Tanvir Hasan Ratul <tanvirhasan58p@gmail.com>
2026-09-20 01:06:33 +0600

    Time: 0 ms (100%), Space: 21.6 MB (34.93%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
-
-    bool dfs(TreeNode* root, int targetSum, stack<int>& st, int currentSum) {
-
-        if (root == nullptr) {
+     bool dfs(TreeNode* root,int targetSum,stack<int>&st,int currentSum){
+        if(!root){
             return false;
         }
-
-        // Add current node
         st.push(root->val);
-        currentSum += root->val;
-
-        // Leaf node
-        if (root->left == nullptr && root->right == nullptr) {
-
-            if (currentSum == targetSum) {
+        currentSum+=root->val;
+        if(!root->left && !root->right){
+            if(currentSum == targetSum){
                 st.pop();
                 return true;
             }
         }
-
-        // Left subtree
-        if (root->left != nullptr) {
-            if (dfs(root->left, targetSum, st, currentSum)) {
+        if(root->left){
+            if(dfs(root->left,targetSum,st,currentSum)){
                 st.pop();
                 return true;
             }
         }
-
-        // Right subtree
-        if (root->right != nullptr) {
-            if (dfs(root->right, targetSum, st, currentSum)) {
+        if(root->right){
+            if(dfs(root->right,targetSum,st,currentSum)){
                 st.pop();
                 return true;
             }
         }
-
-        // Backtrack
         st.pop();
-
         return false;
-    }
-
-    bool hasPathSum(TreeNode* root, int targetSum) {
-
-        stack<int> st;
-
-        return dfs(root, targetSum, st, 0);
+     }
+     bool hasPathSum(TreeNode* root, int targetSum) {
+        stack<int>st;
+        return dfs(root,targetSum,st,0);
     }
 };
